e6876c6ce54c3fa1630b6e20b53d53486d15a5d8
nathancolesol <DunlowSpaeth05@hotmail.com>
2025-05-06 07:48:31 +0800

    feat(core): add fill() to Sprite; handy for backgrounds

--- src/sprite.js
+++ src/sprite.js
@@ -4,6 +4,14 @@ export class Sprite {
     this.height = height;
     this.pixels = Array.from({ length: height }, () => Array(width).fill(0));
   }
+  fill(color) {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        this.pixels[y][x] = color;
+      }
+    }
+    return this;
+  }
   setPixel(x, y, color) {
     if (x<0||y<0||x>=this.width||y>=this.height) return false;
     this.pixels[y][x] = color;
