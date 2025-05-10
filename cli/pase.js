64d704a5e08cf08545d01a87adfc9cdf3d296545
nathancolesol <DunlowSpaeth05@hotmail.com>
2025-05-10 22:03:19 +0800

    feat(cli): add fill command; simple JSON persistence

--- cli/pase.js
+++ cli/pase.js
@@ -2,11 +2,24 @@
 import { Sprite } from '../src/sprite.js';
 import fs from 'fs';
 const [,,cmd,...args]=process.argv;
+
+function save(sprite, file='sprite.json'){
+  fs.writeFileSync(file, JSON.stringify({w:sprite.width,h:sprite.height,p:sprite.pixels}));
+}
+
 if (cmd==='init'){
   const [w,h]=args.map(Number);
   const s=new Sprite(w||16,h||16);
-  fs.writeFileSync('sprite.json', JSON.stringify({w:s.width,h:s.height,p:s.pixels}));
+  save(s);
   console.log('created sprite.json');
+} else if (cmd==='fill'){
+  const [color=1]=args.map(Number);
+  const data = JSON.parse(fs.readFileSync('sprite.json','utf8'));
+  const s=new Sprite(data.w,data.h);
+  s.pixels=data.p;
+  s.fill(color);
+  save(s);
+  console.log('filled with', color);
 } else {
-  console.log('usage: pase init [w h]');
+  console.log('usage: pase <init|fill>');
 }
